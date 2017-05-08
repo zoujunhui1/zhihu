903d09e2c257d8b94742daab9c4cefa68ec3329c
junhui.zou <junhui.zou@wenba100.com>
2017-05-08 17:14:02 +0800

    关注问题 vue.js

--- resources/assets/js/components/QuestionFollowButton.vue
+++ resources/assets/js/components/QuestionFollowButton.vue
@@ -1,14 +1,17 @@
 <template>
-     <button class="btn btn-default" v-text="text">
-
-     </button>
+     <button class="btn btn-default"
+             v-text="text"
+             v-on:click="follow"
+             v-bind:class="{'btn-success':followed}"
+     ></button>
 </template>
 
 <script>
     export default {
+        props:['question','user'],
         mounted(){
-            this.$http.post('/api/question/follower',{}).then(response=>{
-                console.log(response.data);
+            this.$http.post('/api/question/follower',{'question':this.question,'user':this.user}).then(response=>{
+                this.followed = response.data.followed
             })
         },
         data(){
@@ -20,6 +23,13 @@
             text(){
                 return this.followed?'已关注':'关注该问题'
             }
+        },
+        methods:{
+          follow(){
+              this.$http.post('/api/question/follow',{'question':this.question,'user':this.user}).then(response=>{
+                  this.followed = response.data.followed
+          })
+          }
         }
     }
 </script>
